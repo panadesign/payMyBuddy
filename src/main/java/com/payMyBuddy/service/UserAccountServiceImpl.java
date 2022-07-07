74fe24def5f550c72282ae738f4cda2b3c39f5a2
Jeremy <elbarto_860@hotmail.com>
2022-07-07 20:02:03 +0200

    clean

--- src/main/java/com/payMyBuddy/service/UserAccountServiceImpl.java
+++ src/main/java/com/payMyBuddy/service/UserAccountServiceImpl.java
@@ -15,40 +15,41 @@ import java.util.List;
 
 @Service
 @Component
-public class UserAccountServiceImpl implements UserAccountService{
-
+public class UserAccountServiceImpl implements UserAccountService {
+	
+	private final UserAccountRepository userAccountRepository;
 	@Autowired
 	private PasswordEncoder passwordEncoder;
 	@Autowired
 	private MapperService mapperService;
-
-	private final UserAccountRepository userAccountRepository;
-
+	@Autowired
+	private PrincipalUser principalUser;
+	
 	public UserAccountServiceImpl(UserAccountRepository userAccountRepository) {
 		this.userAccountRepository = userAccountRepository;
 	}
-
+	
 	public List<UserAccount> getAllUsersAccount() {
 		return userAccountRepository.findAll();
 	}
-
+	
 	public ProfileDto getUserAccountByEmail(String email) {
 		UserAccount userAccount = userAccountRepository.findByEmail(email)
 				.orElseThrow(() -> new RessourceNotFoundException("User not found with email : " + email));
 		return mapperService.convertUserAccountToProfileDto(userAccount);
 	}
-
+	
 	private boolean emailExist(String email) {
 		return userAccountRepository.findByEmail(email).isPresent();
 	}
-
+	
 	public UserAccount registerNewUserAccount(UserAccountDto userAccountDto) {
-		if(emailExist(userAccountDto.getEmail())) {
+		if (emailExist(userAccountDto.getEmail())) {
 			throw new UserAlreadyExistException("There is an account with that email address:" + userAccountDto.getEmail());
 		}
 		String password = passwordEncoder.encode(userAccountDto.getPassword());
 		UserAccount userAccount = new UserAccount(userAccountDto.getEmail(), userAccountDto.getFirstname(), userAccountDto.getLastname(), password);
-
+		
 		return userAccountRepository.save(userAccount);
 	}
 	
