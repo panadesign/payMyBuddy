2a657eea957be99e2efe767fdb92e4cbe9ae9e4c
Jeremy Charpentier <jeremy.charpentier@posteo.net>
2022-07-29 21:37:11 +0200

    add contact.html

---
+++ src/test/java/com/payMyBuddy/controller/ContactControllerTest.java
@@ -0,0 +1,44 @@
+package com.payMyBuddy.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@SpringBootTest
+@Sql("/test-data.sql")
+@Transactional
+class ContactControllerTest {
+	private MockMvc mockMvc;
+
+	@Autowired
+	private WebApplicationContext webApplicationContext;
+
+	@BeforeEach
+	public void init() {
+		mockMvc = MockMvcBuilders
+				.webAppContextSetup(webApplicationContext)
+				.apply(springSecurity())
+				.build();
+	}
+
+	@Test
+	@WithUserDetails("c.miossec@mail.com")
+	void contact() throws Exception {
+		mockMvc.perform(get("/contact")
+				.with(csrf()))
+				.andExpect(status().isOk());
+	}
+}
