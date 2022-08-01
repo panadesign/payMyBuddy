829e748a5de278427cca35925bd347111b91e4bc
Jeremy Charpentier <jeremy.charpentier@posteo.net>
2022-08-01 09:47:16 +0200

    add contact.html

--- src/main/java/com/payMyBuddy/controller/ContactController.java
+++ src/main/java/com/payMyBuddy/controller/ContactController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ContactController {
 
-	@GetMapping({"/contact", "/"})
-	public String login() {
+	@GetMapping({"/contact"})
+	public String contact() {
 		return "/contact";
 	}
 
