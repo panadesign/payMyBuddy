1f375a7502c1ff3d10c2950e625cdb586e5c59ba
Jeremy Charpentier <jeremy.charpentier@posteo.net>
2022-08-09 10:48:52 +0200

    test inté

--- src/test/java/com/payMyBuddy/service/integration/TransactionServiceIT.java
+++ src/test/java/com/payMyBuddy/service/integration/TransactionServiceIT.java
@@ -4,12 +4,12 @@ import com.payMyBuddy.dao.TransactionRepository;
 import com.payMyBuddy.dao.UserAccountRepository;
 import com.payMyBuddy.model.Account;
 import com.payMyBuddy.model.AccountStatus;
+import com.payMyBuddy.model.Transaction;
 import com.payMyBuddy.model.UserAccount;
 import com.payMyBuddy.service.TransactionService;
 import com.payMyBuddy.service.UserAccountService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -18,7 +18,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.UUID;
 
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 
@@ -31,6 +30,12 @@ class TransactionServiceIT {
 
 	@MockBean
 	UserAccountService userAccountService;
+
+	@MockBean
+	TransactionRepository transactionRepository;
+
+	@Autowired
+	UserAccountRepository userAccountRepository;
 
 	@Test
 	@Transactional
@@ -62,6 +67,26 @@ class TransactionServiceIT {
 		Assertions.assertThrows(IllegalArgumentException.class, () -> transactionService.addMoney(0));
 		Assertions.assertEquals(1000, userAccount.getAccount().getBalance());
 
+	}
+
+	@Test
+	@Transactional
+	void ShouldTransferMoneyToContact() {
+		//GIVEN
+		UserAccount userAccount = new UserAccount(UUID.randomUUID(), "mail@mail.com", "jeremy", "charpentier", "bla", "1234", AccountStatus.ACTIVE, new Account(UUID.randomUUID(), 1000));
+		UserAccount contact = new UserAccount(UUID.randomUUID(), "contact@mail", "contact", "contact", "1", null, AccountStatus.ACTIVE, new Account(UUID.randomUUID(), 0));
+		userAccount.getContactList().add(contact);
+		Transaction transaction = new Transaction(contact.getId(), 200, "test", "EUR", userAccount, contact);
+
+		//WHEN
+		when(userAccountService.getPrincipalUser()).thenReturn(userAccount);
+		when(transactionRepository.save(transaction)).thenReturn(transaction);
+
+		transactionService.transferMoney(transaction.getId(), transaction.getAmount(), transaction.getDescription());
+
+		//THEN
+		Assertions.assertEquals(200, contact.getAccount().getBalance());
+		Assertions.assertEquals(799, userAccount.getAccount().getBalance());
 	}
 
 }
